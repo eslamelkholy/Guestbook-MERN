e865b93d0327d12a5b8b1f7ec71e2d6c0d92c86b
eslam <eslamelkholy5325762@gmail.com>
2020-05-12 02:57:07 +0200

    Add Message Replies

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,10 +1,10 @@
 const mongoose=require("mongoose");
+var ObjectId = mongoose.Schema.Types.ObjectId;
 const userSchema=new mongoose.Schema({
     username:{ type: String, required: true},
     email:{ type : String, require: true, index:true, unique:true,sparse:true},
     password:{type:String, require:true},
-    messages: [{type: Number, ref: "Messages"}],
-    replies: [{type: Number, ref: "Replies"}]
+    messages: [{type: ObjectId, ref: "Messages"}],
 });
 
 //mapping

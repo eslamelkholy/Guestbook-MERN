a2b4adf359409b895efb8eec16fb2e7c97437220
eslam <eslamelkholy5325762@gmail.com>
2020-05-12 23:48:19 +0200

    Home Page UI

--- client/src/MessageComponent/messageHomePage.js
+++ client/src/MessageComponent/messageHomePage.js
@@ -1,10 +1,27 @@
 import React, { Fragment } from 'react'
+import '../layout/messageStyle/messageHome.css'
+import { Link } from 'react-router-dom';
 class MessageHomePage extends React.Component {
     render() {
         return (
             <Fragment>
-                <br/><br/>
-                <h1>Welcome To Guestbook Login Page</h1>
+                <h2>Welcome To Guestbook </h2>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-7">
+                            <div class="card">
+                                <div class="card-header">
+                                    Username
+                                    <Link to="/message/:id" class="btn btn-info controllers">Show</Link>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Hello This Message</h5>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Fragment>
         )
     }
